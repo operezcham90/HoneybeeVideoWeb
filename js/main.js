b4d47e8cd6bb3377719c4f8c819d062b6b50b3a4
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-07 15:46:57 -0500

    --

--- js/main.js
+++ js/main.js
@@ -3,13 +3,22 @@
 // unlock screen
 $("#loading").hide();
 
-// load an image file
-$("#loader").prop("src", "img/lol.png");
+// load image files
+$("#frame1").prop("src", "img/lol.png");
+$("#frame0").prop("src", "img/lol_l.png");
 
-// convert to mat
-$("#loader").on("load", function () {
-    let mat = cv.imread($("#loader").get(0));
-    cv.imshow("output", mat);
-    $("#mu_e").val(mat.at(cv.Point(10, 10)));
-    mat.delete();
+// wait for image
+var frame0_loaded = false;
+var frame1_loaded = false;
+$("#frame0").on("load", function () {
+    frame0_loaded = true;
 });
+$("#frame1").on("load", function () {
+    frame1_loaded = true;
+});
+
+// main loop
+setInterval(main, 60);
+function main() {
+    $("#output_text").val(frame0_loaded + " " + frame1_loaded);
+}
