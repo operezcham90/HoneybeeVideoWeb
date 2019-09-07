4931f1415a4edf086ffb329c25a36cc8e899f901
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-07 16:13:32 -0500

    --

--- js/main.js
+++ js/main.js
@@ -17,16 +17,29 @@ $("#frame1").on("load", function () {
     frame1_loaded = true;
 });
 
+// global variables
+var src;
+var templ;
+var dst;
+var mask;
+
 // main loop
 var state = 0;
 setInterval(main, 60);
 function main() {
     if (frame0_loaded && frame1_loaded && state === 0) {
         $("#output_text").append("Image frames loaded!\n");
+        src = cv.imread('frame1');
+        templ = cv.imread('frame0');
+        dst = new cv.Mat();
+        mask = new cv.Mat();
+        cv.imshow('output0', src);
+        cv.imshow('output1', templ);
         state = 1;
     }
     if (state === 1) {
         $("#output_text").append("Using NCC...\n");
+        cv.matchTemplate(src, templ, dst, cv.TM_CCORR_NORMED, mask);
         state = 2;
     }
 }
