817330122b02cba9889c3965546e1356bcf56d67
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-07 14:20:21 -0500

    --

--- js/main.js
+++ js/main.js
@@ -10,5 +10,6 @@ $("#loader").prop("src", "img/lol.png");
 $("#loader").on("load", function () {
     let mat = cv.imread($("#loader").get(0));
     cv.imshow("output", mat);
+    $("#mu_e").val(mat.at(cv.Point(10, 10)));
     mat.delete();
 });
