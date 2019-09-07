1dfee5bb9b6b0fd6c912fc4857c4d66d75af638e
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-07 13:55:46 -0500

    --

--- js/main.js
+++ js/main.js
@@ -1,2 +1,12 @@
+/* global cv */
+
+// unlock screen
 $("#loading").hide();
+
+// load an image file
 $("#loader").prop("src", "img/lol.png");
+
+// convert to mat
+let mat = cv.imread($("#loader"));
+cv.imshow($("#output"), mat);
+mat.delete();
