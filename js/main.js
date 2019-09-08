1e79bd9023660a9521dc8faa59acf18aaa6b6f2d
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-08 12:09:10 -0500

    --

--- js/main.js
+++ js/main.js
@@ -12,10 +12,17 @@ var frame0_loaded = false;
 var frame1_loaded = false;
 $("#frame0").on("load", function () {
     frame0_loaded = true;
+    check_loading();
 });
 $("#frame1").on("load", function () {
     frame1_loaded = true;
+    check_loading();
 });
+function check_loading() {
+    if (frame0_loaded && frame1_loaded) {
+
+    }
+}
 
 // global variables
 var src;
@@ -23,35 +30,26 @@ var templ;
 var dst;
 var mask;
 
-// main loop
-var state = 0;
-setInterval(main, 60);
+// main function
 function main() {
-    if (frame0_loaded && frame1_loaded && state === 0) {
-        $("#output_text").append("Image frames loaded!\n");
-        src = cv.imread('frame1');
-        templ = cv.imread('frame0');
-        dst = new cv.Mat();
-        mask = new cv.Mat();
-        state = 1;
-    }
-    if (state === 1) {
-        $("#output_text").append("Using NCC...\n");
-        cv.matchTemplate(src, templ, dst, cv.TM_CCORR_NORMED, mask);
-        state = 2;
-    }
-    if (state === 2) {
-        state = 3;
-        let result = cv.minMaxLoc(dst, mask);
-        let maxPoint = result.maxLoc;
-        let maxVal = result.maxVal;
-        $("#output_text").append("Max: " + maxVal + " (" + maxPoint.x + "," + maxPoint.y + ")\n");
-        let color = new cv.Scalar(255, 0, 0, 255);
-        let point = new cv.Point(maxPoint.x + templ.cols, maxPoint.y + templ.rows);
-        cv.rectangle(src, maxPoint, point, color, 2, cv.LINE_8, 0);
-        cv.imshow('output0', src);
-        cv.imshow('output1', templ);
-        cv.imshow('output2', dst);
-        cv.imshow('output0', src);
-    }
+    src = cv.imread('frame1');
+    templ = cv.imread('frame0');
+    dst = new cv.Mat();
+    mask = new cv.Mat();
+    $("#output_text").append("Image frames loaded!\n");
+    
+    $("#output_text").append("Using NCC...\n");
+    cv.matchTemplate(src, templ, dst, cv.TM_CCORR_NORMED, mask);
+    let result = cv.minMaxLoc(dst, mask);
+    let maxPoint = result.maxLoc;
+    let maxVal = result.maxVal;
+    
+    $("#output_text").append("Max: " + maxVal + " (" + maxPoint.x + "," + maxPoint.y + ")\n");
+    let color = new cv.Scalar(255, 0, 0, 255);
+    let point = new cv.Point(maxPoint.x + templ.cols, maxPoint.y + templ.rows);
+    cv.rectangle(src, maxPoint, point, color, 2, cv.LINE_8, 0);
+    cv.imshow('output0', src);
+    cv.imshow('output1', templ);
+    cv.imshow('output2', dst);
+    cv.imshow('output0', src);
 }
