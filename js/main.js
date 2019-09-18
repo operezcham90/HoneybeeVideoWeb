898c5b16641dca193c7b71754a143fa0aaa1d517
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-18 18:54:36 -0500

    --

--- js/main.js
+++ js/main.js
@@ -1,1 +1,6 @@
-alert('Hola');
+var img = new Image();
+img.src = 'img/test.png';
+var canvas = document.createElement('canvas');
+var context = canvas.getContext('2d');
+context.drawImage(img, 0, 0);
+alert(JSON.stringify(context.getImageData(2, 2, 1, 1).data));
