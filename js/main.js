ea300007b6b22a308a2fe18ba4bc4375e5db5fb4
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-19 09:14:23 -0500

    --

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-function read_frame(url) {
-    var frame = {
-        img: new Image(),
-        ctx: document.createElement('canvas'),
-        get_color: function (x, y) {
-            return this.ctx.getImageData(x, y, 1, 1).data;
-        }
-    };
-    frame.img.src = url;
-    frame.ctx.drawImage(frame.img, 0, 0, frame.img.width, frame.img.height);
-    return frame;
+// global variables
+var images = [];
+var canvases = [];
+
+function load_frames() {
+    for (var i = 0; i < 2; i++) {
+        images[i] = new Image();
+        images[i].id = JSON.stringify({'img': 0});
+        images[i].src = 'img/test.png';
+        images[i].onload = loaded_frame;
+    }
 }
 
-/*var img = new Image();
- img.src = 'img/test.png';
- img.onload = function () {
+function loaded_frame() {
+    alert(this.id);
+}
+
+/*img.onload = function () {
  var canvas = document.createElement('canvas');
  var context = canvas.getContext('2d');
  context.drawImage(img, 0, 0, img.width, img.height);
@@ -22,7 +24,7 @@ function read_frame(url) {
  $('#output_text').append(JSON.stringify(context.getImageData(i, j, 1, 1).data));
  }
  }
- }*/
-
-var f = read_frame('img/test.png');
-alert(f.get_color(1, 1));
+ }
+ 
+ var f = read_frame('img/test.png');
+ alert(f.get_color(1, 1));*/
