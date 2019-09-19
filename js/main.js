3a077d7220b008cbf3821213f8c50bfbda76d964
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-18 22:02:08 -0500

    --

--- js/main.js
+++ js/main.js
@@ -2,6 +2,6 @@ var img = new Image();
 img.src = 'img/test.png';
 var canvas = document.createElement('canvas');
 var context = canvas.getContext('2d');
-context.drawImage(img, 0, 0);
+context.drawImage(img, 0, 0, img.width, img.height);
 
-$('#output_text').append(JSON.stringify(context.getImageData(0, 0, 3, 3)));
+$('#output_text').append(JSON.stringify(context.getImageData(0, 0, 1, 1)));
