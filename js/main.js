745f1cb7bf844ca36fb48bf15adc62b44beedc40
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-23 17:05:02 -0500

    --

--- js/main.js
+++ js/main.js
@@ -1,121 +1,45 @@
 // honeybee tracker
-var honeybee = {
-    individual: function (space, dimensions) {
-        return {
-            dimensions: dimensions,
-            space: space
-        };
-    },
-    ncc: function (i1, i2) {
-        // starting points
-        var u1 = i1.dimensions[0] + i1.dimensions[2];
-        var v1 = i1.dimensions[1] + i1.dimensions[3];
-        var u2 = i2.dimensions[0] + i2.dimensions[2];
-        var v2 = i2.dimensions[1] + i2.dimensions[3];
-        var nx = i1.dimensions[4];
-        var ny = i1.dimensions[5];
-        var mx = util.relation[i1.space].image.naturalWidth;
-        var my = util.relation[i1.space].image.naturalHeight;
-        // negative values
-        if (nx < 0 || ny < 0 || mx < 0 || my < 0) {
-            return -100;
-        }
-        // window not inside frame
-        if (u1 + nx >= mx || u2 + nx >= mx || v1 + ny >= my || v2 + ny >= my ||
-                u1 < 0 || u2 < 0 || v1 < 0 || v2 < 0) {
-            return -100;
-        }
-        // get mean
-        var mean1 = 0;
-        var mean2 = 0;
-        for (var x = 0; x < nx; x++) {
-            for (var y = 0; y < ny; y++) {
-                mean1 += util.pixel(i1.space, u1 + x, v1 + y);
-                mean2 += util.pixel(i2.space, u2 + x, v2 + y);
-            }
-        }
-        mean1 /= nx * ny;
-        mean2 /= nx * ny;
-        // get cross correlation and sums of squared errors
-        var cross = 0;
-        var sigma1 = 0;
-        var sigma2 = 0;
-        for (var x = 0; x < nx; x++) {
-            for (var y = 0; y < ny; y++) {
-                var err1 = util.pixel(i1.space, u1 + x, v1 + y) - mean1;
-                var err2 = util.pixel(i2.space, u2 + x, v2 + y) - mean2;
-                cross += err1 * err2;
-                sigma1 += err1 * err1;
-                sigma2 += err2 * err2;
-            }
-        }
-        // only real numbers
-        if (sigma1 < 0 || sigma2 < 0 || sigma1 * sigma2 <= 0) {
-            return -100;
-        }
-        // result
-        return cross / Math.sqrt(sigma1 * sigma2);
-    }
-}
-// utilities
-var util = {
+var hb = {
     loaded: 0,
-    relation: [
-        {
-            file: 'img/00000001.jpg',
-            label: 'f',
-            canvas: null,
-            image: null
-        },
-        {
-            file: 'img/00000001.jpg',
-            label: 't',
-            canvas: null,
-            image: null
-        }
-    ],
-    output: function (text) {
-        // print text
-        $('#output').append(text + '\n');
-    },
-    pixel: function (space, x, y) {
-        // get pixel from canvas
-        return (util.relation[space].canvas.getContext('2d')
-                .getImageData(x, y, 1, 1).data[0] +
-                util.relation[space].canvas.getContext('2d')
-                .getImageData(x, y, 1, 1).data[1] +
-                util.relation[space].canvas.getContext('2d')
-                .getImageData(x, y, 1, 1).data[2]) / 3;
+    spaces: [],
+    space: function (file) {
+        // create a search space
+        var i = new Image();
+        i.id = 'c-' + hb.spaces.length;
+        i.src = file;
+        i.onload = hb.load;
+        hb.spaces.push({
+            file: file,
+            canvas: document.createElement('canvas'),
+            image: i
+        });
     },
     load: function () {
         // load frames
         var i = this.id.split('-')[1];
-        util.relation[i].canvas = document.createElement('canvas');
-        util.relation[i].canvas.getContext('2d')
-                .drawImage(util.relation[i].image, 0, 0);
-        loaded++;
-        if (loaded === util.relation.length) {
-            util.output('All frames loaded');
-            util.process();
+        hb.spaces[i].canvas.getContext('2d')
+                .drawImage(hb.spaces[i].image, 0, 0);
+        hb.loaded++;
+        if (hb.loaded === hb.spaces.length) {
+            hb.output('All frames loaded');
+            hb.process();
         }
     },
     read: function () {
-        // read frames
-        loaded = 0;
-        for (var i = 0; i < util.relation.length; i++) {
-            util.relation[i].image = new Image();
-            util.relation[i].image.id = 'c-' + i;
-            util.relation[i].image.src = util.relation[i].file;
-            util.relation[i].image.onload = util.load;
+        // read two frames
+        hb.loaded = 0;
+        for (var i = 0; i < 2; i++) {
+            hb.spaces = [];
+            space('img/0000000' + (i + 1) + '.jpg');
         }
     },
-    process: function () {
-        util.output('Process started');
-        var i1 = honeybee.individual(0, [139.52, 58.571, 0, 0, 226.67 - 139.52, 148.57 - 58.571]);
-        var i2 = honeybee.individual(1, [139.52, 58.571, 0, 0, 226.67 - 139.52, 148.57 - 58.571]);
-        var ncc = honeybee.ncc(i1, i2);
-        util.output('NCC:' + ncc);
+    output: function (text) {
+        // print text
+        $('#output').append(text + '\n');
+    },
+    process() {
+
     }
 };
 // begin
-$(document).ready(util.read);
+$(document).ready(hb.read);
