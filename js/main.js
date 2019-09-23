2e4869f35a5612b43b5c7fb13cee04a56a40603e
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-22 20:12:01 -0500

    --

--- js/main.js
+++ js/main.js
@@ -1,41 +1,49 @@
-// global variables
-var images = [];
-var canvases = [];
-var loaded_images = 0;
-
 // utilities
-var Util = {
+var util = {
+    loaded: 0,
+    relation: [
+        {
+            file: 'img/test.png',
+            label: 'f',
+            canvas: null,
+            image: null
+        },
+        {
+            file: 'img/test.png',
+            label: 't',
+            canvas: null,
+            image: null
+        }
+    ],
     output: function (text) {
+        // print text
         $('#output').append(text + '\n');
+    },
+    pixel: function (canvas, x, y) {
+        // get pixel from canvas
+        return canvas.context.getImageData(x, y, 1, 1).data;
+    },
+    load: function () {
+        var i = this.id.split('-')[1];
+        relation[i].canvas = document.createElement('canvas');
+        var context = relation[i].canvas.getContext('2d');
+        context.drawImage(relation[i].image, 0, 0);
+        loaded++;
+        if (loaded === util.relation.length) {
+            output('All frames loaded');
+        }
+    },
+    read: function () {
+        // read frames
+        loaded = 0;
+        for (var i = 0; i < util.relation.length; i++) {
+            relation[i].image = new Image();
+            relation[i].image.id = 'c-' + i;
+            relation[i].image.src = relation[i].file;
+            relation[i].image.onload = load;
+        }
     }
 };
 
-// get pixel color
-function get_color(canvas, x, y) {
-    return canvas.context.getImageData(x, y, 1, 1).data;
-}
-
-// load all the frames
-function load_frames() {
-    loaded_images = 0;
-    for (var i = 0; i < 2; i++) {
-        images[i] = new Image();
-        images[i].id = JSON.stringify({'img': i});
-        images[i].src = 'img/test.png';
-        images[i].onload = loaded_frame;
-    }
-}
-
-// wait for the loaded frame
-function loaded_frame() {
-    var i = JSON.parse(this.id).img;
-    canvases[i] = document.createElement('canvas');
-    var context = canvases[i].getContext('2d');
-    context.drawImage(images[i], 0, 0);
-    loaded_images++;
-    if (loaded_images === images.length) {
-        output('All frames loaded');
-    }
-}
-
-$(document).ready(load_frames);
+// begin
+$(document).ready(util.read);
