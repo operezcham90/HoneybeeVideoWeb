76a568abdd2eaadc0e2b0cfce628999419d5251e
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-24 11:35:43 -0500

    --

--- js/main.js
+++ js/main.js
@@ -105,7 +105,7 @@ var hb = {
     },
     process: function () {
         hb.output('Process started');
-        for (var a = 0; a < 64; a++) {
+        for (var a = 0; a < 3; a++) {
             setTimeout(function () {
                 var i1 = hb.individual(0, [139.52, 58.571, 0, 0, 226.67 - 139.52, 148.57 - 58.571]);
                 var i2 = hb.individual(1, [139.52 + a, 58.571, 0, 0, 226.67 - 139.52, 148.57 - 58.571]);
