8c01af1d2e02b769b5f0a9d61a02b540e3353edd
Oscar <Oscar@DESKTOP-4K34HTC>
2019-09-28 19:06:41 -0500

    --

--- js/main.js
+++ js/main.js
@@ -1,29 +1,66 @@
 'use strict'
 // honeybee tracker
 var hb = {
-    loaded: 0,
-    start: new Date(),
-    end: new Date(),
-    ui: 0,
-    vi: 0,
-    hi: 0,
-    wi: 0,
-    mu: 0,
-    lambda: 1,
-    mulambda: 2,
-    gens: 0,
-    eta: {
-        m: 0,
-        c: 0
+    ini: {
+        // initial
+        u: 0,
+        v: 0,
+        h: 0,
+        w: 0,
+        gens: 0,
+        eta: {
+            m: 0,
+            c: 0
+        },
+        ex: {
+            mu: 0,
+            lambda: 0,
+            cross: 0,
+            mut: 0,
+            rand: 0
+        },
+        fo: {
+            mu: 0,
+            lambda: 0,
+            cross: 0,
+            mut: 0,
+            rand: 0
+        },
+        read: function () {
+            // variables
+            hb.ini.gens = 3;
+            hb.ini.ex.mu = 100;
+            hb.ini.ex.lambda = hb.ini.ex.mu * 2;
+            hb.ini.ex.cross = 0.1;
+            hb.ini.ex.mut = 0.6;
+            hb.ini.ex.rand = 0.3;
+            hb.ini.eta.m = 25;
+            hb.ini.eta.c = 2;
+            // initial known position
+            hb.ini.u = Math.ceil(139.52);
+            hb.ini.v = Math.ceil(58.571);
+            hb.ini.w = Math.ceil(226.67 - 139.52);
+            hb.ini.h = Math.ceil(148.57 - 58.571);
+        }
     },
-    exp: {
+    time: {
+        // measure time
+        s: new Date(),
+        e: new Date(),
+        start: function () {
+            hb.time.s = new Date();
+        },
+        end: function () {
+            hb.time.e = new Date();
+            return hb.time.e - hb.time.s;
+        }
+    },
+    pop: {
         mu: 0,
-        lambda: 0,
-        cross: 0,
-        mut: 0,
-        rand: 0
+        lambda: 1,
+        mulambda: 2,
+        arr: [[], [], []]
     },
-    populations: [[], [], []],
     limits: [
         {min: 0, max: 0},
         {min: 0, max: 0},
@@ -32,7 +69,10 @@ var hb = {
         {min: 0, max: 0},
         {min: 0, max: 0}
     ],
-    spaces: [],
+    spaces: {
+        loaded: 0,
+        arr: []
+    },
     tour: {
         pos: 0,
         list: [],
@@ -79,9 +119,9 @@ var hb = {
     parents: function (size) {
         // limits of the search space
         hb.limits[0].min = 0;
-        hb.limits[0].max = hb.spaces[1].image.naturalWidth - hb.wi - 1;
+        hb.limits[0].max = hb.spaces.arr[1].image.naturalWidth - hb.wi - 1;
         hb.limits[1].min = 0;
-        hb.limits[1].max = hb.spaces[1].image.naturalHeight - hb.hi - 1;
+        hb.limits[1].max = hb.spaces.arr[1].image.naturalHeight - hb.hi - 1;
         hb.limits[2].min = 0;
         hb.limits[2].max = hb.wi - 1;
         hb.limits[3].min = 0;
@@ -130,12 +170,31 @@ var hb = {
         }
         return d;
     },
+    beta: function (u) {
+        var b;
+        if (1 - u < 1e-6) {
+            u = 1 - 1e-6;
+        }
+        if (u < 0) {
+            u = 0;
+        }
+        if (u < 0.5) {
+            b = Math.pow(2 * u, (1 / (hb.eta.c + 1)));
+        } else {
+            b = Math.pow((0.5 / (1 - u)), (1 / (hb.eta.c + 1)));
+        }
+        return b;
+    },
     mutation: function (i) {
         // for each site
-        for (var s = 0; s < hb.populations[hb.lambda][i].dim.length; s++) {
+        for (var s = 0; s < hb.populations[hb.lambda][i].dim.length - 2; s++) {
             // get value
             var x = hb.populations[hb.lambda][i].dim[s];
             var r = hb.limits[s].max - hb.limits[s].min;
+            if (s >= 4) {
+                // dependent components
+                r -= hb.populations[hb.lambda][i].dim[s - 2];
+            }
             // get distance min
             var l = (hb.limits[s].min - x) / r;
             if (l < -1.0) {
@@ -143,6 +202,11 @@ var hb = {
             }
             // get distance max
             var h = (hb.limits[s].max - x) / r;
+            if (s >= 4) {
+                // dependent components
+                h = (hb.limits[s].max - x -
+                        hb.populations[hb.lambda][i].dim[s - 2]) / r;
+            }
             if (h > 1.0) {
                 h = 1.0;
             }
@@ -162,6 +226,79 @@ var hb = {
             }
             if (hb.populations[hb.lambda][i].dim[s] > hb.limits[s].max) {
                 hb.populations[hb.lambda][i].dim[s] = hb.limits[s].max;
+            }
+        }
+    },
+    crossover: function (m1, m2) {
+        // create children
+        hb.populations[hb.lambda].push(hb.individual(1, [0, 0, 0, 0, 0, 0]));
+        hb.populations[hb.lambda].push(hb.individual(1, [0, 0, 0, 0, 0, 0]));
+        var c1 = hb.populations[hb.lambda].length - 2;
+        var c2 = hb.populations[hb.lambda].length - 1;
+        for (var s = 0; s < hb.populations[hb.mu][m1].dim.length; s++) {
+            // parents
+            var p1 = hb.populations[hb.mu][m1].dim[s];
+            var p2 = hb.populations[hb.mu][m2].dim[s];
+            // limits
+            var l = hb.limits[s].min;
+            var h = hb.limits[s].max;
+            // check order
+            var o = false;
+            if (p1 > p2) {
+                var t = p1;
+                p1 = p2;
+                p2 = t;
+                o = true;
+            }
+            // mean and difference
+            var m = (p1 + p2) * 0.5;
+            var d = p2 - p1;
+            // check limits
+            var r = 0;
+            if ((p1 - l) < (h - p2)) {
+                r = p1 - l;
+            } else {
+                r = h - p2;
+            }
+            if (r < 0) {
+                r = 0;
+            }
+            // check difference
+            var u = 0;
+            if (d > 1e-6) {
+                var a = 1 + (2 * r / d);
+                var um = 1 - (0.5 / Math.pow(a, hb.eta.c + 1));
+                u = um * Math.random();
+            } else {
+                u = Math.random();
+            }
+            // get beta
+            var b = hb.beta(u);
+            if (Math.abs(d * b) > Number.POSITIVE_INFINITY) {
+                b = Number.POSITIVE_INFINITY / d;
+            }
+            // get values
+            var v2 = m + (b * 0.5 * d);
+            var v1 = m - (b * 0.5 * d);
+            // limits
+            if (v2 < l) {
+                v2 = l;
+            }
+            if (v2 > h) {
+                v2 = h;
+            }
+            if (v1 < l) {
+                v1 = l;
+            }
+            if (v1 > h) {
+                v1 = h;
+            }
+            // children
+            hb.populations[hb.lambda][c2].dim[s] = v2;
+            hb.populations[hb.lambda][c1].dim[s] = v1;
+            if (o) {
+                hb.populations[hb.lambda][c2].dim[s] = v1;
+                hb.populations[hb.lambda][c1].dim[s] = v2;
             }
         }
     },
@@ -197,6 +334,12 @@ var hb = {
             // mutate
             hb.mutation(hb.populations[hb.lambda].length - 1);
         }
+        // crossover
+        for (var i = 0; i < nc; i += 2) {
+            var m1 = hb.tour.select();
+            var m2 = hb.tour.select();
+            hb.crossover(m1, m2);
+        }
     },
     merge: function () {
         return 0;
@@ -213,17 +356,16 @@ var hb = {
                 hb.populations[pop][i].dim[5]
             ]);
             // comparison
-            hb.populations[pop][i].fit
-                    = hb.ncc(hb.populations[pop][i], i2);
+            hb.populations[pop][i].fit = hb.ncc(hb.populations[pop][i], i2);
         }
     },
     space: function (file) {
         // create a search space
         var i = new Image();
-        i.id = 'c-' + hb.spaces.length;
+        i.id = 'c-' + hb.spaces.arr.length;
         i.src = file;
         i.onload = hb.load;
-        hb.spaces.push({
+        hb.spaces.arr.push({
             file: file,
             canvas: document.createElement('canvas'),
             image: i,
@@ -233,27 +375,28 @@ var hb = {
     load: function () {
         // load frames
         var i = this.id.split('-')[1];
-        hb.spaces[i].canvas.getContext('2d')
-                .drawImage(hb.spaces[i].image, 0, 0);
-        var data = hb.spaces[i].canvas
+        hb.spaces.arr[i].canvas.getContext('2d')
+                .drawImage(hb.spaces.arr[i].image, 0, 0);
+        var data = hb.spaces.arr[i].canvas
                 .getContext('2d')
                 .getImageData(0, 0,
-                        hb.spaces[i].image.naturalWidth,
-                        hb.spaces[i].image.naturalHeight).data;
+                        hb.spaces.arr[i].image.naturalWidth,
+                        hb.spaces.arr[i].image.naturalHeight).data;
         for (var j = 0; j < (data.length / 4); j++) {
             var b = j * 4;
-            hb.spaces[i].gray[j] = (data[b] + data[b + 1] + data[b + 2]) / 3;
+            hb.spaces.arr[i].gray[j] =
+                    (data[b] + data[b + 1] + data[b + 2]) / 3;
         }
-        hb.loaded++;
-        if (hb.loaded === 2) {
+        hb.spaces.loaded++;
+        if (hb.spaces.loaded === 2) {
             hb.output('All frames loaded');
             hb.process();
         }
     },
     read: function () {
         // read two frames
-        hb.loaded = 0;
-        hb.spaces = [];
+        hb.spaces.loaded = 0;
+        hb.spaces.arr = [];
         for (var i = 0; i < 2; i++) {
             hb.space('img/0000000' + (i + 1) + '.jpg');
         }
@@ -277,8 +420,8 @@ var hb = {
         var v2 = Math.floor(i2.dim[1] + i2.dim[3]);
         var nx = Math.floor(i1.dim[4]);
         var ny = Math.floor(i1.dim[5]);
-        var mx = hb.spaces[i1.spc].image.naturalWidth;
-        var my = hb.spaces[i1.spc].image.naturalHeight;
+        var mx = hb.spaces.arr[i1.spc].image.naturalWidth;
+        var my = hb.spaces.arr[i1.spc].image.naturalHeight;
         // negative values
         if (nx < 0 || ny < 0 || mx < 0 || my < 0) {
             return -100;
@@ -289,8 +432,8 @@ var hb = {
             return -100;
         }
         // get pixel data
-        var p1 = hb.spaces[i1.spc].gray;
-        var p2 = hb.spaces[i2.spc].gray;
+        var p1 = hb.spaces.arr[i1.spc].gray;
+        var p2 = hb.spaces.arr[i2.spc].gray;
         // get mean
         var mean1 = 0;
         var mean2 = 0;
@@ -321,32 +464,19 @@ var hb = {
     },
     process: function () {
         // start timing
-        hb.start = new Date();
+        hb.time.start();
         // configuration
-        hb.gens = 3;
-        hb.exp.mu = 100;
-        hb.exp.lambda = hb.exp.mu * 2;
-        hb.exp.cross = 0.1;
-        hb.exp.mut = 0.6;
-        hb.exp.rand = 0.3;
-        hb.eta.m = 25;
-        hb.eta.c = 2;
-        // initial known position of the object
-        hb.ui = Math.ceil(139.52);
-        hb.vi = Math.ceil(58.571);
-        hb.wi = Math.ceil(226.67 - 139.52);
-        hb.hi = Math.ceil(148.57 - 58.571);
+        hb.ini.read();
         // exploration
-        hb.parents(hb.exp.mu);
-        hb.evaluate(hb.mu);
-        for (var i = 0; i < hb.gens; i++) {
-            hb.offspring(hb.exp.lambda, hb.exp.cross, hb.exp.mut, hb.exp.rand);
-            hb.evaluate(hb.lambda);
+        hb.parents(hb.ini.ex.mu);
+        hb.evaluate(hb.pop.mu);
+        for (var i = 0; i < hb.ini.gens; i++) {
+            hb.offspring(hb.ex.lambda, hb.ex.cross, hb.ex.mut, hb.ex.rand);
+            hb.evaluate(hb.pop.lambda);
             hb.merge();
         }
         // end timing
-        hb.end = new Date();
-        hb.output('Time: ' + (hb.end - hb.start) + ' ms');
+        hb.output('Time: ' + hb.time.end() + ' ms');
     }
 };
 // begin
